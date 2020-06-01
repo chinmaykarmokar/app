d17e3d8669887f00a8454704c4685af8da49a070
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2020-06-02 01:32:02 +0530

    Updates

---
+++ precache-manifest.c72c3ba9a23850707fe03246801e1cdb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98baa4ecadea0f516ae417bcdbb093e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53e872604317d9076db",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a522db2d563247d3322a",
+    "url": "/static/css/main.5d781302.chunk.css"
+  },
+  {
+    "revision": "e53e872604317d9076db",
+    "url": "/static/js/2.20fc7bcc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.20fc7bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a522db2d563247d3322a",
+    "url": "/static/js/main.9e617acd.chunk.js"
+  },
+  {
+    "revision": "233dea908d4de0135036",
+    "url": "/static/js/runtime-main.c8273c4c.js"
+  },
+  {
+    "revision": "817ee04d4cb2aa076991fc0fc4fad910",
+    "url": "/static/media/desk.817ee04d.png"
+  },
+  {
+    "revision": "13cbf53a4c92f00fe47afed14bfc53ad",
+    "url": "/static/media/prof.13cbf53a.png"
+  }
+]);
